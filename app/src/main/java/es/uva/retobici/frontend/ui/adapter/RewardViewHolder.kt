56d0078d3c4441adc548bc760d1fcdb3924111c0
jorplaz <429-jorplaz@users.noreply.gitlab.inf.uva.es>
2022-06-16 15:04:12 +0200

    Glide library to load internet image url's

--- app/src/main/java/es/uva/retobici/frontend/ui/adapter/RewardViewHolder.kt
+++ app/src/main/java/es/uva/retobici/frontend/ui/adapter/RewardViewHolder.kt
@@ -3,6 +3,7 @@ package es.uva.retobici.frontend.ui.adapter
 import android.view.View
 import android.widget.Toast
 import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
 import com.mapbox.navigation.examples.databinding.ItemRewardBinding
 import es.uva.retobici.frontend.domain.model.Reward
 
@@ -17,7 +18,8 @@ class RewardViewHolder(
     fun render(reward: Reward, onClickListener: (Reward) -> Unit){
         binding.rewardTitle.text = reward.title
         binding.rewardPoints.text = reward.points.toString()
-        binding.rewardDate.text = "Fecha de fin: 12/11/1997"
+        binding.rewardDate.text = "Válido hasta: 12/11/2022"
+        Glide.with(binding.imageReward.context).load(reward.image).into(binding.imageReward)
 
         //If the reward have been obtained can not be obtained again
         if (reward.obtained){
